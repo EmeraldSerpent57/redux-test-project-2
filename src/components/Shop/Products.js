18d28f98c28b41fead2bfc97f1080d425e81ff1f
Alaina Harrison <amvoce1221@gmail.com>
2022-11-28 13:49:19 -0800

    added hard coded products array

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,53 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 6,
+    name: "Sci-fi Book",
+    description: "An alien utopia!",
+  },
+  {
+    id: "p2",
+    price: 8,
+    name: "Romance Book",
+    description: "Love, hardships, and an unforgettable ending!",
+  },
+  {
+    id: "p3",
+    price: 7,
+    name: "Self-Help Book",
+    description: "Want to better yourself? Read this!",
+  },
+  {
+    id: "p4",
+    price: 8,
+    name: "Fantasy Book",
+    description: "Vampires, Fairies, and Werewolves, OH MY!",
+  },
+  {
+    id: "p5",
+    price: 10,
+    name: "Cook Book",
+    description: "Easy and delicious recipies!",
+  },
+];
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.id}
+            name={product.name}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
+        
       </ul>
     </section>
   );
