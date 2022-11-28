33aeaf24d42a424ebda89cdced619b7c8ce523c2
Alaina Harrison <amvoce1221@gmail.com>
2022-11-28 13:34:40 -0800

    added removeItemFromCart func to cartSlice

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-createSlice({
+const cartSlice = createSlice({
     name: 'cart',
     initialState: {
         items: [],
@@ -26,6 +26,20 @@ createSlice({
                 existingItem.totalPrice = existingItem.totalPrice + newItem.price;
             }
         },
-        removeItemFromCart() {},
+        removeItemFromCart(state, action) {
+            const id = action.payload;
+            //how many items are in the array?
+            const existingItem = state.items.find(item => item.id === id);
+            if (existingItem.quantity === 1) {
+                state.items = state.items.filter(item => item.id !== id);
+            } else {
+                existingItem.quantity--;
+                existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+            }
+        },
     },
 });
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice;
