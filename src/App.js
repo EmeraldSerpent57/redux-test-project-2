5617af5a105dec8cd376f91ffd38504030eced82
Alaina Harrison <amvoce1221@gmail.com>
2022-11-29 10:48:07 -0800

    async request with useEffect in src/App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
@@ -6,6 +7,16 @@ import Products from './components/Shop/Products';
 
 function App() {
   const showCart = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector(state => state.cart);
+
+  //watch for changes in cart state
+  useEffect(() => {
+    fetch(
+      "https://redux-test-project-2-default-rtdb.firebaseio.com/cart.json",
+      { method: "PUT", body: JSON.stringify(cart) }
+    );
+  }, [cart]);
+
   return (
     <Layout>
       {showCart && <Cart />}
@@ -15,3 +26,7 @@ function App() {
 }
 
 export default App;
+
+
+/* NOTE: using a 'PUT' request will override the cart of existing 
+data with the incoming data */
