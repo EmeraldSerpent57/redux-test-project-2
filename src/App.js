5ade27102d625b9ff9c1958bdbd2bf69b40e66e1
Alaina Harrison <amvoce1221@gmail.com>
2022-11-29 22:46:42 -0800

    added action creator thunk for sending data to firebase

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
-import { useSelector } from 'react-redux';
+import { Fragment, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import Notification from './components/UI/Notification';
+import { sendCartData } from './store/cart-slice';
+
+let isInitial = true; 
 
 function App() {
-  const showCart = useSelector(state => state.ui.cartIsVisible);
+  const dispatch = useDispatch();
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+
+  //watch for changes in cart state
+  useEffect(() => {
+    //if the cart is just being loaded, no cart data will be sent. only does this once!
+    if (isInitial) {
+      isInitial = false;
+      return;
+    };
+
+    dispatch(sendCartData(cart));
+  }, [cart, dispatch]);
+
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
 export default App;
+
+/* NOTE: using a 'PUT' request will override the cart of existing 
+data with the incoming data */
+
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,5 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+import { uiActions } from './ui-slice';
+
 const cartSlice = createSlice({
     name: 'cart',
     initialState: {
@@ -41,6 +43,54 @@ const cartSlice = createSlice({
         },
     },
 });
+
+export const sendCartData = (cart) => {
+    return async (dispatch) => {
+        //shows first that the data in the cart is pending being sent
+        dispatch(
+            uiActions.showNotification({
+                status: "pending",
+                title: "Sending...",
+                message: "Sending cart data now!",
+            })
+        );
+
+        const sendRequest = async () => {
+          //then we send the data to the firebase backend
+          const response = await fetch(
+            "https://redux-test-project-2-default-rtdb.firebaseio.com/cart.json",
+            { method: "PUT", body: JSON.stringify(cart) }
+          );
+
+          //is there an error?
+          if (!response.ok) {
+            throw new Error("Sending cart data failed!");
+          }
+        };
+
+    try{
+        await sendRequest();
+        //notification that data send was successful
+       dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Cart data has been sent successfully!",
+        })
+      );
+    } catch (error) {
+        //catch any error that may have occurred and show error message
+        dispatch(
+            uiActions.showNotification({
+              status: "error",
+              title: "Error!",
+              message: "Sending cart data failed!",
+            })
+          );
+    }
+
+    };
+};
 
 export const cartActions = cartSlice.actions;
 
