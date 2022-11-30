116be86539ab66e82aaa91f3d687120dc2b64274
Alaina Harrison <amvoce1221@gmail.com>
2022-11-30 14:12:18 -0800

    added func to fetch cart data from firebase

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -1,5 +1,37 @@
 import { uiActions } from './ui-slice';
+import { cartActions } from './cart-slice';
 
+//async func for fetching stored data from firebase
+export const fetchCartData = () => {
+    return async (dispatch) => {
+        const fetchData = async () => {
+            const response = await fetch("https://redux-test-project-2-default-rtdb.firebaseio.com/cart.json");
+            
+            if (!response.ok) {
+                throw new Error('Could not fetch cart data.');
+            }
+
+            const data = await response.json();
+
+            return data;
+        };
+
+        try {
+            const cartData = await fetchData();
+            dispatch(cartActions.replaceCart(cartData));
+        } catch (error) {
+            dispatch(
+                uiActions.showNotification({
+                  status: "error",
+                  title: "Error!",
+                  message: "Fetching cart data failed.",
+                })
+            );
+        }
+    };
+};
+
+//async func for sending cart data to firebase
 export const sendCartData = (cart) => {
     return async (dispatch) => {
         //shows first that the data in the cart is pending being sent
